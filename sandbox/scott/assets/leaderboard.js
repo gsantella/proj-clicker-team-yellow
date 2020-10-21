d45d8bb3d2d6710b6f7b7759ee5ee024d6f5f33b
S-goss <70651880+S-goss@users.noreply.github.com>
2020-10-21 15:14:35 -0400

    Leaderboards fix

--- sandbox/scott/assets/leaderboard.js
+++ sandbox/scott/assets/leaderboard.js
@@ -1,13 +1,13 @@
 const app = new Vue({
     el: '#app',
     data: {
-      leaderboards:[]
+      leaderboards: []
     },
     created () {
         fetch('https://run.mocky.io/v3/2aca2482-2984-4cee-986b-1144596da3eb')
         .then(reposnse => reposnse.json())
         .then(json => {
-            this.leaderboards = json.leaderboards
+            this.leaderboards = json.leaderboard
         })
 
     }
